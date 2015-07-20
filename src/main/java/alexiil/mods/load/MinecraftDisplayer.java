06184f25af3a7e8e47256482d9f986d7e7c6ccd9
AlexIIL <alexj9@me.com>
2015-07-20 19:49:34 +0100

    Actually fix alpha

--- src/main/java/alexiil/mods/load/MinecraftDisplayer.java
+++ src/main/java/alexiil/mods/load/MinecraftDisplayer.java
@@ -290,7 +290,9 @@ public class MinecraftDisplayer implements IDisplayer {
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
         GL11.glClearColor(clearRed, clearGreen, clearBlue, 1);
 
-        GL11.glEnable(GL11.GL_ALPHA);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+
         GL11.glEnable(GL11.GL_ALPHA_TEST);
         GL11.glAlphaFunc(GL11.GL_GREATER, 0.1F);
 
